3f086c1fe9c7890fd29c7926c7c16c16a0ac6dfe
zkywalker230061 <3132647228@qq.com>
2025-10-15 15:24:27 +0100

    get monthly average - completed

--- Chengyun/rgargo_analysis.py
+++ Chengyun/rgargo_analysis.py
@@ -5,7 +5,7 @@ Chengyun Zhu
 2025-10-12
 """
 
-from IPython.display import display
+# from IPython.display import display
 
 # import xarray as xr
 # import pandas as pd
@@ -65,28 +65,116 @@ def get_monthly_mean(
     return monthly_mean_da
 
 
-def main():
-    """Main function for rgargo_analysis.py."""
+def save_monthly_mean_anomalies():
+    """Save the monthly mean of the anomaly dataset."""
 
-    # ds_temp = load_and_prepare_dataset(
-    #     "../datasets/RG_ArgoClim_Temperature_2019.nc",
-    # )
+    ds_temp = load_and_prepare_dataset(
+        "../datasets/RG_ArgoClim_Temperature_2019.nc",
+    )
     # display(ds_temp)
-    # visualise_dataset(ds_temp)  # Call Kal'tsit.
-
-    # ta = ds_temp['ARGO_TEMPERATURE_ANOMALY']
+    ta = ds_temp['ARGO_TEMPERATURE_ANOMALY']
     # display(ta)
-    # ta_monthly_mean = get_monthly_mean(ta)
+    ta_monthly_mean = get_monthly_mean(ta)
     # display(ta_monthly_mean)
-    # visualise_dataset(ta_monthly_mean.sel(
-    #     PRESSURE=0, MONTH=1, method='nearest')
+    # visualise_dataset(
+    #     ta_monthly_mean.sel(PRESSURE=0, MONTH=1, method='nearest')
     # )
-    # ta_monthly_mean.to_netcdf("../datasets/ARGO_TEMPERATURE_ANOMALY_Monthly_Mean.nc")
+    ta_monthly_mean.to_netcdf("../datasets/ARGO_TEMPERATURE_ANOMALY_Monthly_Mean.nc")
+
+    ds_salt = load_and_prepare_dataset(
+        "../datasets/RG_ArgoClim_Salinity_2019.nc",
+    )
+    # display(ds_salt)
+    sa = ds_salt['ARGO_SALINITY_ANOMALY']
+    # display(sa)
+    sa_monthly_mean = get_monthly_mean(sa)
+    # display(sa_monthly_mean)
+    # visualise_dataset(
+    #     sa_monthly_mean.sel(PRESSURE=0, MONTH=1, method='nearest')
+    # )
+    sa_monthly_mean.to_netcdf("../datasets/ARGO_SALINITY_ANOMALY_Monthly_Mean.nc")
+
+
+def save_monthly_mean_temperature():
+    """Save the monthly mean temperature dataset."""
 
     ta_monthly_mean = load_and_prepare_dataset(
         "../datasets/ARGO_TEMPERATURE_ANOMALY_Monthly_Mean.nc",
+    )['Monthly Mean of ARGO_TEMPERATURE_ANOMALY']
+    # display(ta_monthly_mean)
+    # visualise_dataset(
+    #     ta_monthly_mean.sel(PRESSURE=0, MONTH=1, method='nearest')
+    # )
+    t_15years_mean = load_and_prepare_dataset(
+        "../datasets/RG_ArgoClim_Temperature_2019.nc",
+    )['ARGO_TEMPERATURE_MEAN']
+    # display(t_15years_mean)
+    # visualise_dataset(
+    #     t_15years_mean.sel(PRESSURE=0, method='nearest')
+    # )
+
+    # add two toghether, monthly mean have dimension MONTH, 15 years mean not
+    t_15years_mean = t_15years_mean.expand_dims(MONTH=12)
+    t_monthly_mean = t_15years_mean + ta_monthly_mean
+
+    t_monthly_mean.attrs['units'] = ta_monthly_mean.attrs.get('units')
+    t_monthly_mean.attrs['long_name'] = (
+        "Monthly Mean of Temperature Jan 2004 - Dec 2018 (15.0 year)"
     )
-    display(ta_monthly_mean)
+    t_monthly_mean.name = "Monthly Mean of Temperature"
+    # display(t_monthly_mean)
+    # visualise_dataset(
+    #     t_monthly_mean.sel(PRESSURE=0, MONTH=1, method='nearest')
+    # )
+    # visualise_dataset(
+    #     t_monthly_mean.sel(MONTH=1, LONGITUDE=0, LATITUDE=0, method='nearest')
+    # )
+    t_monthly_mean.to_netcdf("../datasets/Temperature_Monthly_Mean.nc")
+
+
+def save_monthly_mean_salinity():
+    """Save the monthly mean salinity dataset."""
+
+    sa_monthly_mean = load_and_prepare_dataset(
+        "../datasets/ARGO_SALINITY_ANOMALY_Monthly_Mean.nc",
+    )['Monthly Mean of ARGO_SALINITY_ANOMALY']
+    # display(sa_monthly_mean)
+    # visualise_dataset(
+    #     sa_monthly_mean.sel(PRESSURE=0, MONTH=1, method='nearest')
+    # )
+    s_15years_mean = load_and_prepare_dataset(
+        "../datasets/RG_ArgoClim_Salinity_2019.nc",
+    )['ARGO_SALINITY_MEAN']
+    # display(s_15years_mean)
+    # visualise_dataset(
+    #     s_15years_mean.sel(PRESSURE=0, method='nearest')
+    # )
+
+    # add two toghether, monthly mean have dimension MONTH, 15 years mean not
+    s_15years_mean = s_15years_mean.expand_dims(MONTH=12)
+    s_monthly_mean = s_15years_mean + sa_monthly_mean
+
+    s_monthly_mean.attrs['units'] = sa_monthly_mean.attrs.get('units')
+    s_monthly_mean.attrs['long_name'] = (
+        "Monthly Mean of Salinity Jan 2004 - Dec 2018 (15.0 year)"
+    )
+    s_monthly_mean.name = "Monthly Mean of Salinity"
+    # display(s_monthly_mean)
+    # visualise_dataset(
+    #     s_monthly_mean.sel(PRESSURE=0, MONTH=1, method='nearest')
+    # )
+    # visualise_dataset(
+    #     s_monthly_mean.sel(MONTH=1, LONGITUDE=0, LATITUDE=0, method='nearest')
+    # )
+    s_monthly_mean.to_netcdf("../datasets/Salinity_Monthly_Mean.nc")
+
+
+def main():
+    """Main function for rgargo_analysis.py."""
+    # save_monthly_mean_anomalies()
+
+    # save_monthly_mean_temperature()
+    # save_monthly_mean_salinity()
 
 
 if __name__ == "__main__":
