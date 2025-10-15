b0a46f8f455f8f990ba67b028a707fa2101d079b
jky521 <jy521@ic.ac.uk>
2025-10-15 20:33:53 +0100

    Added function to convert depth into xarray

--- Jason/rg_sst_map/calculte_Tm.py
+++ Jason/rg_sst_map/calculte_Tm.py
@@ -6,14 +6,48 @@ h_meters = 100.0
 ZDIM = "PRESSURE"
 YDIM = "LATITUDE"
 XDIM = "LONGITUDE"
+TDIM = "TIME"
 T_VAR = "ARGO_TEMPERATURE_MEAN"
 S_VAR = "ARGO_SALINITY_MEAN"
 T_VAR_ANOMALY = "ARGO_TEMPERATURE_ANOMALY"
 
+def z_to_xaray(z,
+               T: xr.DataArray,
+               zdim: str = ZDIM,
+               ydim: str = YDIM,
+               xdim: str = XDIM,
+               tdim: str = TDIM) -> xr.DataArray:
+    """
+    Returns an xarray DataArray of depth z (in meters) broadcast to the shape of T.
+    """
+    if not isinstance(z, xr.DataArray):
+        z = np.asarray(z)
+    # Z array from gsw has shape (Latitude, Depth)
+        z_new = xr.DataArray(z, dims=[ydim, zdim],
+                             coords={ydim: T.coords[ydim],
+                                     zdim: T.coords[zdim]})
+    else:
+        z_new = z
+    
+    # Ensure z_new has the required dimensions
+    if zdim not in z_new.dims or ydim not in z_new.dims:
+        raise ValueError(f"{zdim} or {ydim} not in {z_new.dims}")
+    
+    # Expand z_new to include missing dimensions in T
+    if xdim in T.dims and xdim not in z_new.dims:
+        z_new = z_new.expand_dims({xdim: T.coords[xdim]})
+    if tdim in T.dims and tdim not in z_new.dims:
+        z_new = z_new.expand_dims({tdim: T.coords[tdim]})
+
+    z_broadcast = z_new.broadcast_like(T)
+    return z_broadcast
+
+
+
 # Using 1D Trapezoidal rule to calculate the vertical integral
 def vertical_integral(
     T: xr.DataArray,
-    z: xr.DataArray,
+    z,
     top: float = 2.5,
     bottom: float = -h_meters,
     normalise: str = "available",
@@ -25,8 +59,10 @@ def vertical_integral(
     # Ensure both inputs T and Z hae the vertical dimensions last in their order of dimensions
     if zdim not in T.dims:
         raise ValueError(f"{zdim} not in T.dims")
+    
+     
     T_sorted = T.transpose(..., zdim)
-    z_sorted = z.transpose(..., zdim)
+    z_sorted = z.broadcast_like(T_sorted).transpose(..., zdim)
 
     # Creating segments of adjacent levels
     T_next = T_sorted.shift({zdim: -1})
@@ -60,5 +96,5 @@ def vertical_integral(
     "long_name": f"Upper {int(abs(bottom))} m mean temperature (trapezoidal)",
     "units": T.attrs.get("units", "degC")
     })
-    
+
     return out
