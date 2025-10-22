d4a84aa31adc5de9e2a26588dd9f5c485744d529
jky521 <jy521@ic.ac.uk>
2025-10-22 18:23:11 +0100

    not working

--- Jason/rg_sst_map/running_Tm.py
+++ Jason/rg_sst_map/running_Tm.py
@@ -4,9 +4,10 @@ Jilia Xie
 
 #%%
 from read_nc import fix_rg_time, fix_longitude_coord
-from calculate_Tm_Sm import depth_from_pressure, _full_field
+from calculate_Tm_Sm import depth_dbar_to_meter, _full_field
 from calculate_Tm_Sm import z_to_xarray
 from calculate_Tm import vertical_integral
+from grad_field import compute_gradients
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -20,12 +21,10 @@ import matplotlib.pyplot as plt
 
 #---1. ---Read File--------------------------------------
 
-# temp_file_path = "C:\Msci Project\RG_ArgoClim_Temperature_2019.nc"
-# salinity_file_path = "C:\Msci Project\RG_ArgoClim_Salinity_2019.nc"
-# updated_h_file_path = "C:\Msci Project\Mixed_Layer_Depth_Pressure (2004-2018).nc"
-temp_file_path = "/Users/xxz/Desktop/SSTA/datasets/RG_ArgoClim_Temperature_2019.nc"
-salinity_file_path = "/Users/xxz/Desktop/SSTA/datasets/RG_ArgoClim_Salinity_2019.nc"
-updated_h_file_path = "/Users/xxz/Desktop/SSTA/datasets/Mixed_Layer_Depth_Pressure (2004-2018).nc"
+temp_file_path = r"C:\Users\jason\MSciProject\RG_ArgoClim_Temperature_2019.nc"
+salinity_file_path = r"C:\Users\jason\MSciProject\RG_ArgoClim_Salinity_2019.nc"
+updated_h_file_path = r"C:\Users\jason\MSciProject\Mixed_Layer_Depth_Pressure (2004-2018).nc"
+
 
 
 ds_temp = xr.open_dataset(
@@ -35,12 +34,12 @@ ds_temp = xr.open_dataset(
     mask_and_scale=True,
 )
 
-ds_sal = xr.open_dataset(
-    salinity_file_path,
-    engine="netcdf4",
-    decode_times=False,
-    mask_and_scale=True,
-)
+# ds_sal = xr.open_dataset(
+#     salinity_file_path,
+#     engine="netcdf4",
+#     decode_times=False,
+#     mask_and_scale=True,
+# )
 
 height_grid = xr.open_dataset(
     updated_h_file_path,
@@ -68,12 +67,12 @@ T_full = fix_longitude_coord(T_full)
 print('T_full:\n',T_full)
 print(T_full.shape)
 print('h_normal:\n',h_normal)
-
+print(T_full.coords)
 #%%
 #----3. gsw--------------------------------------------
 p = ds_temp['PRESSURE']
 lat = ds_temp['LATITUDE']
-depth = depth_from_pressure(p,lat)
+depth = depth_dbar_to_meter(p,lat)
 
 
 h_meters = 100.0
@@ -91,7 +90,11 @@ print('z_new:\n',z_new)
 #%%
 #----4. Vertical Integration -------------------------------
 vertical = vertical_integral(T_full,z_new, h_normal)          #??????i changed here to -z_new
-print('vertical_integral:\n',vertical_integral)
+#%%
+print('vertical_integral:\n',vertical)
+#%%
+gradients = compute_gradients(vertical)
+print('gradients:\n',gradients)
 # #%%
 # test1= T_full.transpose(...,ZDIM)
 # test2 = h_normal.broadcast_like(test1)
@@ -103,19 +106,19 @@ print('vertical_integral:\n',vertical_integral)
 #%%
 if __name__ == "__main__":
     #----Plot Map----------------------------------------------------
-    t0 = vertical.sel(TIME="2009-10-01")
+    t0 = gradients.sel(TIME="2009-10-01")
 
     # Copy the colormap and set NaN color
     cmap = plt.get_cmap("RdYlBu_r").copy()
-    cmap.set_bad(color="gray")   # or "white", "black", (0.5,0.5,0.5,1), etc.
+    cmap.set_bad(color="black")   # or "white", "black", (0.5,0.5,0.5,1), etc.
 
     plt.figure(figsize=(10,5))
     pc = plt.pcolormesh(
         t0["LONGITUDE"], t0["LATITUDE"], np.ma.masked_invalid(t0),
-        cmap=cmap, shading="auto", vmin=-2, vmax=30
+        cmap=cmap, shading="auto"
     )
     plt.colorbar(pc, label="Mean Temperature (°C, 0–100 m)")
-    plt.title("Upper 100 m Mean Temperature - Jan 2006")
+    plt.title("Upper 100 m Mean Temperature Gradient (Lat)- Jan 2006")
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     plt.tight_layout()
