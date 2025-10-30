3e085c4e0d4929866ce419d62421cf678308a089
Christopher O'Sullivan <christopherosullivan@tutanota.com>
2025-10-30 14:54:58 +0000

    Heat flux simulation over time, including animation. I think there is a problem with how the heat flux anomaly is calculated; I get temperature anomalies up to 10^13 K which is obviously nonsense

--- Chris/heat_flux_simulation.py
+++ Chris/heat_flux_simulation.py
@@ -4,16 +4,63 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 import esmpy as ESMF
-from utils import get_monthly_mean, get_anomaly
+from utils import get_monthly_mean, get_anomaly, load_and_prepare_dataset
+from matplotlib.animation import FuncAnimation
+import matplotlib
+matplotlib.use('TkAgg')
 
 HEAT_FLUX_DATA_PATH = "../datasets/heat_flux_interpolated.nc"
 TEMP_DATA_PATH = "../datasets/RG_ArgoClim_Temperature_2019.nc"
+MLD_DATA_PATH = "../datasets/Mixed_Layer_Depth_Pressure-(2004-2018).nc"
 
 heat_flux_ds = xr.open_dataset(HEAT_FLUX_DATA_PATH, decode_times=False)
 heat_flux_ds['NET_HEAT_FLUX'] = heat_flux_ds['slhf'] + heat_flux_ds['sshf']
-temperature_ds = xr.open_dataset(TEMP_DATA_PATH, decode_times=False)
+temperature_ds = load_and_prepare_dataset(TEMP_DATA_PATH)
+#print(temperature_ds)
 heat_flux_monthly_mean = get_monthly_mean(heat_flux_ds['NET_HEAT_FLUX'])
 heat_flux_anomaly_ds = get_anomaly(heat_flux_ds, 'NET_HEAT_FLUX', heat_flux_monthly_mean)
-print(heat_flux_anomaly_ds)
+#print(heat_flux_anomaly_ds)
+mld_ds = xr.open_dataset(MLD_DATA_PATH, decode_times=False)
+#print(mld_ds)
+# ds = xr.concat([heat_flux_anomaly_ds, mld_ds], 'TIME')
+# print(ds)
+
+model_anomalies = []
+added_baseline = False
+for month in heat_flux_anomaly_ds.TIME.values:
+    if not added_baseline:
+        base = temperature_ds.sel(PRESSURE=2.5, TIME=month)['ARGO_TEMPERATURE_ANOMALY']
+        base = base.expand_dims(TIME=[month])  # <-- keep its time
+        model_anomalies.append(base)
+        added_baseline = True
+    else:
+        prev = model_anomalies[-1].isel(TIME=-1)
+        cur = prev + (30.4375 * 24 * 60 * 60) * heat_flux_anomaly_ds.sel(TIME=month)['NET_HEAT_FLUX_ANOMALY'] / mld_ds.sel(TIME=month)['MLD_PRESSURE']
+        cur = cur.expand_dims(TIME=[month])
+        model_anomalies.append(cur)
+model_anomaly_ds = xr.concat(model_anomalies, 'TIME')
+model_anomaly_ds = model_anomaly_ds.drop_vars(["PRESSURE"])
+print(model_anomaly_ds)
+
+#model_anomaly_ds.sel(TIME=100.5).plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r')
+#plt.show()
+
+# make a movie
+times = model_anomaly_ds.TIME.values
+
+fig, ax = plt.subplots()
+pcolormesh = ax.pcolormesh(model_anomaly_ds.LONGITUDE.values, model_anomaly_ds.LATITUDE.values, model_anomaly_ds.isel(TIME=0), cmap='RdBu_r')
+title = ax.set_title(f'Time = {times[0]}')
+
+plt.colorbar(pcolormesh, ax=ax, label='Modelled anomaly from surface heat flux')
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Latitude')
 
 
+def update(frame):
+    pcolormesh.set_array(model_anomaly_ds.isel(TIME=frame).values.ravel())
+    title.set_text(f'Time = {times[frame]}')
+    return [pcolormesh, title]
+
+animation = FuncAnimation(fig, update, frames=len(times), interval=300, blit=True)
+plt.show()
