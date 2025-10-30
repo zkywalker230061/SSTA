1c0b66010cabbe6ab8baa68b8a9bc5282aeb25f5
zkywalker230061 <3132647228@qq.com>
2025-10-30 23:21:39 +0000

    Holte's datset

--- Chengyun/test.py
+++ Chengyun/test.py
@@ -5,16 +5,38 @@ Chengyun Zhu
 2025-10-11
 """
 
+from IPython.display import display
 
-from rgargo_read import load_and_prepare_dataset
-from rgargo_plot import visualise_dataset
+import xarray as xr
+import matplotlib.pyplot as plt
+# import cartopy.crs as ccrs
+
+# from rgargo_read import load_and_prepare_dataset
+# from rgargo_plot import visualise_dataset
 
 
 def main():
-    t_m = load_and_prepare_dataset(
-        "../dataset/Mean Temperature Dataset (2004-2018).nc"
+    """Main function."""
+    ds = xr.open_dataset("../datasets/Argo_mixedlayers_monthlyclim_04142022.nc")
+    # ds = xr.open_dataset("../datasets/Argo_mixedlayers_all_04142022.nc")
+    ds = ds.rename({'iLAT': 'LATITUDE', 'iLON': 'LONGITUDE', 'iMONTH': 'MONTH'})
+    display(ds)
+
+    MONTH = 1
+
+    ds['mld_da_mean'].sel(MONTH=MONTH).plot(
+        figsize=(12, 6), cmap='Blues', levels=200,
+        vmin=0, vmax=500
     )
-    visualise_dataset(t_m)
+    plt.show()
+    print(ds['mld_da_mean'].sel(MONTH=MONTH).max().max().item())
+
+    ds['mld_dt_mean'].sel(MONTH=MONTH).plot(
+        figsize=(12, 6), cmap='Blues', levels=200,
+        vmin=0, vmax=500
+    )
+    plt.show()
+    print(ds['mld_dt_mean'].sel(MONTH=MONTH).max().max().item())
 
 
 if __name__ == "__main__":
