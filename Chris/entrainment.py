b366440200e4cbca3401724877fd8502e0eaeef7
Christopher O'Sullivan <christopherosullivan@tutanota.com>
2025-11-05 14:14:27 +0000

    Get anomaly of T_sub. Seems to be 0 anomaly remarkably often...

--- Chris/entrainment.py
+++ Chris/entrainment.py
@@ -15,9 +15,6 @@ ta = temp_ds["ARGO_TEMPERATURE_ANOMALY"]
 tm = tm.expand_dims(TIME=180)
 temp_ds["TEMPERATURE"] = tm + ta
 
-print(temp_ds)
-#print(mld_ds)
-
 
 def get_T_sub(temp_ds, mld_ds, month, make_plots=True):
     temp_ds = temp_ds.sel(TIME=month)
@@ -37,16 +34,25 @@ def get_T_sub(temp_ds, mld_ds, month, make_plots=True):
 
     T_sub = xr.apply_ufunc(interpolation, temp_ds['PRESSURE'], temp_ds['TEMPERATURE'], mld_ds['MLD_PRESSURE'], input_core_dims=[['PRESSURE'], ['PRESSURE'], []], vectorize=True)
     mld_ds['T_sub'] = T_sub
-    #temp_ds['T_sub'] = temp_ds['T_sub'].where(temp_ds['T_sub'] <= 500, 500)  # for a better scale
     if make_plots:
         mld_ds['T_sub'].plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r')
         plt.show()
     return mld_ds
 
+# combine dataset from each of the months
 datasets_over_time = []
 for month in temp_ds.TIME.values:
     print(month)
-    datasets_over_time.append(get_T_sub(temp_ds, mld_ds, month, make_plots=True))
+    datasets_over_time.append(get_T_sub(temp_ds, mld_ds, month, make_plots=False))
 t_sub_ds = xr.concat(datasets_over_time, "TIME")
+t_sub_ds = t_sub_ds.drop_vars("MLD_PRESSURE")
+
+# get mean and anomaly
+t_sub_monthly_mean = get_monthly_mean(t_sub_ds['T_sub'])
+t_sub_ds = get_anomaly(t_sub_ds, 'T_sub', t_sub_monthly_mean)
+
 t_sub_ds.to_netcdf("../datasets/t_sub.nc")
 print(t_sub_ds)
+
+t_sub_ds['T_sub_ANOMALY'].sel(TIME=121.5).plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r')
+plt.show()
