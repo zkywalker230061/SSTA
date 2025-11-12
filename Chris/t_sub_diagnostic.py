725631c3adaa20b46724fd9f8e4ed88bacb6aaff
Christopher O'Sullivan <christopherosullivan@tutanota.com>
2025-11-12 13:33:46 +0000

    Found a mishandling of indices when getting T_sub. Before was getting the near-surface temperature, and sometimes giving very weird behaviour. Now should handle the interpolation business correctly.

--- Chris/t_sub_diagnostic.py
+++ Chris/t_sub_diagnostic.py
@@ -6,15 +6,19 @@ from utils import get_monthly_mean, get_anomaly, load_and_prepare_dataset
 T_SUB_DATA_PATH = "../datasets/t_sub.nc"
 
 t_sub_ds = xr.open_dataset(T_SUB_DATA_PATH, decode_times=False)
+# t_sub_ds['T_sub'] = t_sub_ds['SUB_TEMPERATURE']
+# t_sub_monthly_mean = get_monthly_mean(t_sub_ds['T_sub'])
+# t_sub_ds = get_anomaly(t_sub_ds, 'T_sub', t_sub_monthly_mean)
+# t_sub_ds.to_netcdf("../datasets/t_sub_2.nc")
 
 vmin_anom = -1
 vmax_anom = 1
 
-# t_sub_ds['T_sub'].sel(TIME=132.5).plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r')
-# plt.show()
-#
-# t_sub_ds['T_sub'].sel(TIME=138.5).plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r')
-# plt.show()
+t_sub_ds['T_sub'].sel(TIME=132.5).plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r', vmin=0, vmax=30)
+plt.show()
+
+t_sub_ds['T_sub'].sel(TIME=138.5).plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r', vmin=0, vmax=30)
+plt.show()
 
 t_sub_ds['T_sub_ANOMALY'].sel(TIME=0.5).plot(x='LONGITUDE', y='LATITUDE', cmap='RdBu_r', vmin=vmin_anom, vmax=vmax_anom)
 plt.show()
