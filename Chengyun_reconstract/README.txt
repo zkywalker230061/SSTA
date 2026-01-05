543869dd3d3809749fc7d031f61baf29775dc904
zkywalker230061 <3132647228@qq.com>
2026-01-05 15:40:50 +0000

    reconstruction-ERA5-init

---
+++ Chengyun_reconstract/README.txt
@@ -0,0 +1,4 @@
+Before running any code in this reconstruction folder:
+
+- Make sure that in "logs" folder, 
+    "datasets.txt" exists and is empty. 
---
+++ Chengyun_reconstract/prepare_era5.py
@@ -0,0 +1,11 @@
+"""
+Prepare ERA5 dataset.
+
+Chengyun Zhu
+2026-1-3
+"""
+
+from utilities import load_and_prepare_dataset
+from utilities import get_monthly_mean, get_anomaly
+from utilities import save_file
+
