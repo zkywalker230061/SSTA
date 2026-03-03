afb66ba962bca628720ecaec010cac514f08fc12
zkywalker230061 <3132647228@qq.com>
2026-03-03 22:28:43 +0000

    another reconstruction (kind of)

---
+++ Chengyun_2026-3/__init__.py
@@ -0,0 +1,18 @@
+"""
+SSTA and SSSA model.
+
+Chengyun Zhu
+2026-03-03
+"""
+
+
+def main():
+    """
+    Main function to run the SSTA and SSSA model.
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
