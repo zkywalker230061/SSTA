754907427bd1d19624a4fbcad62f40c722925a53
zkywalker230061 <3132647228@qq.com>
2026-05-21 18:10:59 +0100

    housekeeping2

--- __final_version__/main/calculate_maxgradsub.py
+++ __final_version__/main/calculate_maxgradsub.py
@@ -1,5 +1,5 @@
 """
-Module to calculate max gradient quantity for the sub layer.
+Calculate max gradient quantity for the sub layer.
 
 Chengyun
 2026-3-5
@@ -7,12 +7,60 @@ Chengyun
 
 from utilities import load_and_prepare_dataset
 from utilities import get_monthly_mean, get_anomaly
-# from utilities import save_file
+from utilities import save_file
 
 
 RHO_O = 1025  # kg / m^3
 C_O = 4100  # J / (kg K)
 SECONDS_MONTH = 30 * 24 * 60 * 60  # seconds in a month
+
+
+def save_sub_temperature_maxgrad():
+    """Save the sub-layer temperature dataset from max-gradient method."""
+
+    # TODO
+
+
+def save_monthly_mean_sub_temperature_maxgrad():
+    """Save the monthly mean sub-layer temperature dataset from max-gradient method."""
+
+    with open("logs/datasets.txt", "r", encoding="utf-8") as logs_datasets:
+        if "datasets/Sub_Layer_Temperature_Max_Gradient_Method-Clim_Mean.nc" in logs_datasets.read():
+            return
+
+    t_sub = load_and_prepare_dataset(
+        'datasets/Sub_Layer_Temperature_Max_Gradient_Method-(2004-2025).nc'
+    )['SUB_TEMPERATURE']
+
+    t_sub_monthly_mean = get_monthly_mean(t_sub)
+
+    save_file(
+        t_sub_monthly_mean,
+        'datasets/Sub_Layer_Temperature_Max_Gradient_Method-Clim_Mean.nc'
+    )
+
+
+def save_sub_temperature_anomalies_maxgrad():
+    """Save the sub-layer temperature anomalies dataset from max-gradient method."""
+
+    with open("logs/datasets.txt", "r", encoding="utf-8") as logs_datasets:
+        if "datasets/Sub_Layer_Temperature_Max_Gradient_Method_Anomalies-(2004-2025).nc" in logs_datasets.read():
+            return
+
+    t_sub = load_and_prepare_dataset(
+        'datasets/Sub_Layer_Temperature_Max_Gradient_Method-(2004-2025).nc'
+    )['SUB_TEMPERATURE']
+
+    t_sub_monthly_mean = load_and_prepare_dataset(
+        'datasets/Sub_Layer_Temperature_Max_Gradient_Method-Clim_Mean.nc'
+    )['MONTHLY_MEAN_SUB_TEMPERATURE']
+
+    t_sub_a = get_anomaly(t_sub, t_sub_monthly_mean)
+
+    save_file(
+        t_sub_a,
+        'datasets/Sub_Layer_Temperature_Max_Gradient_Method_Anomalies-(2004-2025).nc'
+    )
 
 
 def main():
@@ -35,6 +83,10 @@ def main():
     t_sub_anomaly = get_anomaly(t_sub, t_sub_monthly_mean)
     t_sub_monthly_mean.to_netcdf("datasets/Sub_Layer_Temperature_Max_Gradient_Method-Clim_Mean.nc")
     t_sub_anomaly.to_netcdf("datasets/Sub_Layer_Temperature_Max_Gradient_Method_Anomalies-(2004-2025).nc")
+
+    save_sub_temperature_maxgrad()
+    save_monthly_mean_sub_temperature_maxgrad()
+    save_sub_temperature_anomalies_maxgrad()
 
 
 if __name__ == "__main__":
